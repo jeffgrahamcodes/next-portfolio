c3aac31202ca27ad08cb7eb79e6ee3fa1651bd9d
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-22 02:18:05 -0400

    add Links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div>
+      <div>
+        <Link href="/about">About</Link>
+        <Link href="/projects">Projects</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
       <h1>HOME</h1>
     </div>
   );
