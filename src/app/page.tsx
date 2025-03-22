929f65c940d962288452ec68429599bcd61f2504
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-22 09:55:39 -0400

    create Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import Image from 'next/image';
+import Hero from '@/components/hero';
 import homeImg from '/public/images/home.jpg';
 
 export default function Home() {
   return (
     <div>
-      <h1>HOME</h1>
-      <div className="absolute -z-10 inset-0">
-        <Image
-          src={homeImg}
-          alt="TODO"
-          fill
-          style={{ objectFit: 'cover' }}
-        />
-      </div>
+      <Hero imgData={homeImg} imgAlt="background" title="Home Page" />
     </div>
   );
 }
