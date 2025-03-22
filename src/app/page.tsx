28ece2d34e7bb7d3f9edeeb0e76ff96d3f8bceac
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-22 02:25:17 -0400

    move Links to layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
     <div>
-      <div>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
-        <Link href="/contact">Contact</Link>
-      </div>
       <h1>HOME</h1>
     </div>
   );
