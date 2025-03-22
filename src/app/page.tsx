1399fc2bab76c952e24c300209f39a7198e5f51d
jeffgrahamcodes <jeffgrahamcodes@gmail.com>
2025-03-22 02:50:00 -0400

    add background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
+import homeImg from '/public/images/home.jpg';
+
 export default function Home() {
   return (
     <div>
       <h1>HOME</h1>
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="TODO"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
     </div>
   );
 }
